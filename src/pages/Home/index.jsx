4a77fc97f1f13c9605ebb632257da93e06264cae
DuMuniz <edu.smuniz@hotmail.com>
2020-08-25 23:32:36 -0300

    Desenvolve conteudo lateral e carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,61 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-const Home = () => <Container>Hello World</Container>;
+
+import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png'
+import { Card } from '../../components';
+
+import { Container, Carousel, Search, Logo, Wrapper, Map, CarrouselTitle } from './styles';
+
+const Home = () => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+      };
+
+    return(
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="Logo do restaurante" />
+                    <TextField
+                        label="Pesquisar Restaurantes"
+                        outlined
+                        // onTrailingIconSelect={() => this.setState({ value: ''})}
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    >
+                        <Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />    
+                    </TextField>
+                    <CarrouselTitle>Na sua Área</CarrouselTitle>
+                    <Carousel {...settings}>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                        <Card photo={restaurante} title="nome restaurante"/>
+                    </Carousel>
+                </Search>    
+            </Container>
+            <Map/>
+        </Wrapper>
+    );
+};
 
 export default Home;
